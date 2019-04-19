2795c85003688bad76232316271549a2d0d0435c
tafkap95 <conroe.consulting@free.fr>
2019-04-19 17:57:14 +0200

    first commit

---
+++ stdafx.h
@@ -0,0 +1,16 @@
+
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN		// Windows
+#define _WIN32_IE 0x0300
+#include <windows.h>
+
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+#include <stdio.h>
+#include <commctrl.h>
+#include <windows.h>
+
+
